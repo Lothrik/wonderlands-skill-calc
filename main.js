ff4bc1bf9731576f12ab8693731a359baf298b57
Lothrik <maximius@gmail.com>
2022-03-10 22:14:14 -0800

    Skill tree style changes

--- main.js
+++ main.js
@@ -290,43 +290,43 @@ function finishHTML() {
 	switch ($("#primaryClassSelector option:selected").val()){
 		default:
 		case "brrzerker":
-			$("#primaryTree .progressLayer").css({ "background-color": "rgba(0, 0, 255, 0.25)" });
+			$("#primaryTree .colorLayer").css({ "background-color": "rgba(0, 0, 255, 0.25)" });
 			break;
 		case "clawbringer":
-			$("#primaryTree .progressLayer").css({ "background-color": "rgba(0, 255, 255, 0.25)" });
+			$("#primaryTree .colorLayer").css({ "background-color": "rgba(0, 255, 255, 0.25)" });
 			break;
 		case "graveborn":
-			$("#primaryTree .progressLayer").css({ "background-color": "rgba(255, 0, 0, 0.25)" });
+			$("#primaryTree .colorLayer").css({ "background-color": "rgba(255, 0, 0, 0.25)" });
 			break;
 		case "spellshot":
-			$("#primaryTree .progressLayer").css({ "background-color": "rgba(255, 0, 255, 0.25)" });
+			$("#primaryTree .colorLayer").css({ "background-color": "rgba(255, 0, 255, 0.25)" });
 			break;
 		case "sporewarden":
-			$("#primaryTree .progressLayer").css({ "background-color": "rgba(0, 255, 0, 0.25)" });
+			$("#primaryTree .colorLayer").css({ "background-color": "rgba(0, 255, 0, 0.25)" });
 			break;
 		case "stabbomancer":
-			$("#primaryTree .progressLayer").css({ "background-color": "rgba(255, 255, 0, 0.25)" });
+			$("#primaryTree .colorLayer").css({ "background-color": "rgba(255, 255, 0, 0.25)" });
 			break;
 	}
 	switch ($("#secondaryClassSelector option:selected").val()){
 		default:
 		case "brrzerker":
-			$("#secondaryTree .progressLayer").css({ "background-color": "rgba(0, 0, 255, 0.25)" });
+			$("#secondaryTree .colorLayer").css({ "background-color": "rgba(0, 0, 255, 0.25)" });
 			break;
 		case "clawbringer":
-			$("#secondaryTree .progressLayer").css({ "background-color": "rgba(0, 255, 255, 0.25)" });
+			$("#secondaryTree .colorLayer").css({ "background-color": "rgba(0, 255, 255, 0.25)" });
 			break;
 		case "graveborn":
-			$("#secondaryTree .progressLayer").css({ "background-color": "rgba(255, 0, 0, 0.25)" });
+			$("#secondaryTree .colorLayer").css({ "background-color": "rgba(255, 0, 0, 0.25)" });
 			break;
 		case "spellshot":
-			$("#secondaryTree .progressLayer").css({ "background-color": "rgba(255, 0, 255, 0.25)" });
+			$("#secondaryTree .colorLayer").css({ "background-color": "rgba(255, 0, 255, 0.25)" });
 			break;
 		case "sporewarden":
-			$("#secondaryTree .progressLayer").css({ "background-color": "rgba(0, 255, 0, 0.25)" });
+			$("#secondaryTree .colorLayer").css({ "background-color": "rgba(0, 255, 0, 0.25)" });
 			break;
 		case "stabbomancer":
-			$("#secondaryTree .progressLayer").css({ "background-color": "rgba(255, 255, 0, 0.25)" });
+			$("#secondaryTree .colorLayer").css({ "background-color": "rgba(255, 255, 0, 0.25)" });
 			break;
 	}
 }
@@ -488,7 +488,7 @@ function updatePassiveSkills(treeHandle) {
 		$(this).attr("data-total", tierTotal);
 	});
 	$(treeHandle).parent().find(".totalPoints").text(totalPoints);
-	$(treeHandle).parent().children(".progressLayer, .progressLine").height(Math.min(75 + totalPoints * 59.0 / 5 + (totalPoints > 25 ? 21 : 0), 396));
+	$(treeHandle).parent().children(".colorLayer, .progressLine").height(Math.min(75 + totalPoints * 59.0 / 5 + (totalPoints > 25 ? 21 : 0), 370));
 }
 function updateStats() {
 	let total = 0;
